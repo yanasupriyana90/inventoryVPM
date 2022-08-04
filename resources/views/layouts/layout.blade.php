d1acf6b755845e0c92ae1b75d9b1910f5c52766f
yanasupriyana90 <yanasupriyana90@gmail.com>
2022-08-04 15:42:10 +0700

    Ada perubahan

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -134,7 +134,7 @@
 						<li class="nav-item {{ Request::is('kategori', 'barang') ? 'active' : '' }}">
 							<a data-toggle="collapse" href="#base">
 								<i class="fas fa-layer-group"></i>
-								<p>Data Master Cobaan</p>
+								<p>Data Master</p>
 								<span class="caret"></span>
 							</a>
 							<div class="collapse" id="base">
